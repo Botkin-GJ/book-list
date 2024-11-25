e23c910135109972b11abc0fe8b3540229186ae2
Kirill Borodkin <borodkin.kirill@outlook.de>
2024-11-25 15:20:04 +0100

    Add book list rendering

--- src/app/features/book-list-display/components/book-list-display-pc/book-list-display-pc.component.ts
+++ src/app/features/book-list-display/components/book-list-display-pc/book-list-display-pc.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { BooksViewModel } from '../../../../shared/models/book.model';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatDivider } from '@angular/material/divider';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-book-list-display-pc',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDivider, TranslatePipe],
+  providers: [],
   templateUrl: './book-list-display-pc.component.html',
-  styleUrl: './book-list-display-pc.component.scss'
+  styleUrl: './book-list-display-pc.component.scss',
 })
 export class BookListDisplayPcComponent {
+  Math = Math;
 
   @Input()
   books: BooksViewModel[] = [];
